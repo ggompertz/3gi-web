cbe5f60b3742273f049b959459809b87f2172f77
Gonzalo Gompertz <gonzalo@3g-ia-agents.com>
2026-05-08 17:58:15 +0000

    chore: eliminar scripts SQL ejecutados

--- docs/supabase/queries_pendientes.sql
+++
@@ -1,8 +0,0 @@
--- Ejecutar cada query por separado en el SQL Editor de Supabase
--- Copiar el resultado y compartirlo para completar fix_security_advisories.sql
-
--- QUERY 1: definición de la vista v_pipeline_clientes
-SELECT pg_get_viewdef('v_pipeline_clientes'::regclass, true);
-
--- QUERY 2: definición de la función match_memories
-SELECT pg_get_functiondef('match_memories'::regproc);
